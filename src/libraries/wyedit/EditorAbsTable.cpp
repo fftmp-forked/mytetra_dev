1c34946d9fb39f742f5cf1d82cd1eb024266c463
fft <none>
2024-11-25 23:38:40 +0400

    f

--- src/libraries/wyedit/EditorAbsTable.cpp
+++ src/libraries/wyedit/EditorAbsTable.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <utility>
 
 #include <QTextCursor>
 #include <QTextDocument>
--- src/libraries/wyedit/EditorAbsTableCell.cpp
+++ src/libraries/wyedit/EditorAbsTableCell.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <utility>
 
 #include "EditorAbsTableCell.h"
 
